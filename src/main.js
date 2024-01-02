65115e1df4b4b47e9f97dd5f55841d7dbc5df6e6
Lourdilene Souza <lourdilene.souza@gmail.com>
2024-01-02 09:34:53 -0300

    Estruturacão inicial do projeto

--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
-import { example } from './dataFunctions.js';
-import { renderItems } from './view.js';
+import { example } from "./dataFunctions.js";
+import { renderItems } from "./view.js";
 
-import data from './data/dataset.js';
+import data from "./data/dataset.js";
 
 console.log(example, renderItems(data), data);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const cards = document.getElementById("card");
+
+  data.forEach((book) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+
+    const titleElement = document.createElement("h2");
+    titleElement.textContent = book.name;
+    cardDiv.appendChild(titleElement);
+
+    const imgElement = document.createElement("img");
+    imgElement.src = book.imageUrl;
+    cardDiv.appendChild(imgElement);
+
+    const descriptionElement = document.createElement("p");
+    descriptionElement.textContent = book.shortDescription;
+    cardDiv.appendChild(descriptionElement);
+
+    cards.appendChild(cardDiv);
+  });
+});
