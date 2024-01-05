539213f35c1fb65ec4f1bad1b8cd3b00adf7b30a
Lourdilene Souza <lourdilene.souza@gmail.com>
2024-01-04 21:16:29 -0300

    two tests

--- src/view.js
+++ src/view.js
@@ -1,6 +1,31 @@
 export const renderItems = (data) => {
-  console.log(data)
+  console.log(data);
   // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
-};
+  const ulElement = document.createElement("ul");
 
+  console.log("array: " + Array.isArray(data));
+  data.forEach((book) => {
+    const liElement = document.createElement("li");
+
+    liElement.setAttribute("itemscope", "");
+    liElement.setAttribute("itemtype", "http://schema.org/Book");
+
+    const titleElement = document.createElement("h2");
+    titleElement.textContent = book.name;
+    titleElement.setAttribute("itemprop", "name");
+    liElement.appendChild(titleElement);
+
+    const imgElement = document.createElement("img");
+    imgElement.src = book.imageUrl;
+    imgElement.setAttribute("itemprop", "image");
+    liElement.appendChild(imgElement);
+
+    const descriptionElement = document.createElement("p");
+    descriptionElement.textContent = book.shortDescription;
+    descriptionElement.setAttribute("itemprop", "description");
+    liElement.appendChild(descriptionElement);
+    ulElement.appendChild(liElement);
+  });
+
+  return ulElement;
+};
