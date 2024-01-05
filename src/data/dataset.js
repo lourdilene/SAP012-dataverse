625a4a689b4c4aeca62b9030ed8cecceefe9b0d5
TaGoulart <tamara.bgg@gmail.com>
2024-01-05 10:38:16 -0300

    teste commit

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -17,18 +17,18 @@ export default [
   },
   {
     id: "2",
-    name: "Testemunha de Acusação II",
+    name: "Nemesis",
     description:
-      "Em um tribunal intrigante, a rainha do crime tece um famoso romance de mistério cheio de surpresas. Este envolvente enigma, repleto de reviravoltas, desdobramentos e personagens cativantes, mantém os leitores à beira de seus assentos enquanto Agatha Christie desvenda uma trama meticulosa. O cenário no tribunal adiciona um toque único à narrativa, proporcionando uma experiência emocionante e imprevisível. Testemunha de Acusação é uma obra-prima compacta que mergulha nas complexidades do sistema judicial, combinando o suspense característico da autora com o fascínio do tribunal, oferecendo uma leitura envolvente e cheia de mistérios.",
+      "No derradeiro livro de Agatha Christie com Miss Marple, a protagonista é surpreendida por uma carta dos advogados de seu falecido amigo Jason Rafiel. Convocada para uma missão enigmática, Jane Marple embarca em uma investigação intrigante, perscrutando pistas obscuras para desvendar o mistério proposto. Publicado em 1971, Nêmesis reintroduz o excêntrico sr. Rafiel, que previamente se unira à astuta senhora em Mistério no Caribe. A trama, surpreendente e perspicaz, fecha com chave de ouro a série de romances estrelados pela astuta Miss Marple, destacando-se como uma conclusão magistral à trajetória da personagem.",
     shortDescription:
-      "Um envolvente drama judicial repleto de mistérios e reviravoltas.",
-    imageUrl: "/images/books/1.jpg",
+      "Nêmesis encerra com maestria a série de romances estrelados por Miss Marple, surpreendente e perspicaz.",
+    imageUrl: "/images/books/2.jpg",
     facts: {
       authorBook: "Agatha Christie",
-      pagesBook: "234",
-      publicationBook: "1994",
+      pagesBook: "301",
+      publicationBook: "1971",
       readingTime: "23",
-      ratingsBook: "3.9",
-    },
+      ratingsBook: "4.5",
+    }
   },
 ];
