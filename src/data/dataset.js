8c93c097fe9c58f3af96d40825a24bd729380ad0
TaGoulart <tamara.bgg@gmail.com>
2024-01-15 14:20:04 -0300

    url image

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -91,7 +91,7 @@ export default [
       "In Agatha Christie's latest plot, Hercule Poirot is summoned to solve a mysterious murder. Renauld, a South American millionaire, is found stabbed near a grave on a golf course. Facing rivals and intrigues, Poirot investigates motives, lovers, and the peculiar familiarity of the case. The competition with a famous French detective adds tension. With unique insight, Poirot unveils hidden secrets, maintaining suspense until the final revelation in this brilliantly captivating Agatha Christie classic, weaving a tale of suspense, deduction, and unexpected twists.",
     shortDescription:
       "In this graphic novel adaptation, detective Hercule Poirot confronts a cunning plot, demanding his utmost sharpness.",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/6.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/6.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "288",
@@ -108,7 +108,7 @@ export default [
       "In this captivating and humorous narrative, Anne's quest for adventure leads her to Hyde Park Corner station. Witness to an apparent fatal accident, she questions Scotland Yard's conclusion of accidental electrocution. Anne suspects foul play. Who was the mysterious man in the brown suit examining the body? His swift escape leaves an enigmatic message. The plot, written with vivacity, unveils an intriguing storyline where the ordinary becomes extraordinary in the pursuit of truth, challenging conventions and leading Anne to unravel mysteries beyond the apparent simplicity of the subway station.",
     shortDescription:
       "Published in 1924, this mystery blends thriller, espionage, and African landscapes, making it a favorite among Agatha Christie fans.",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/7.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/7.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "272",
@@ -125,7 +125,7 @@ export default [
       "Hercule Poirot faces a series of intriguing mysteries, from jewel theft to a 'suicide that turns out to be murder. Cases range from an inexplicably cheap apartment to a prime minister's kidnapping, involving pharaohs' tombs and missing wills. Each enigma challenges Poirot's brilliant deductive powers, revealing surprising connections between mysterious crimes. Throughout fourteen stories, Agatha Christie weaves an intricate plot, keeping readers on the edge of their seats as the famous Belgian detective unravels each intricate puzzle, showing that in the world of crime, nothing is as it seems.",
     shortDescription:
       "Published in 1924, 'Poirot Investigates' features 14 tales where Hercule Poirot solves cases involving greed, jealousy, and revenge.",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/8.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/8.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "399",
@@ -142,7 +142,7 @@ export default [
       "Working in Africa, Anthony Cade accepts a seemingly simple mission but becomes entangled in international conspiracies. Invited to a party at Chimneys mansion, he realizes he's unwittingly complicit in a crime. Collaborating with Superintendent Battle, he faces risky situations to unravel Chimneys' mysteries and prevent further deaths. The plot involves twists, revelations, and action, keeping readers intrigued as Cade confronts the challenges of his unexpected and perilous mission. The novel unfolds with suspense, showcasing the author's skill in crafting engaging and surprising plots.",
     shortDescription:
       "The Secret of Chimneys unveils intrigues with debut Superintendent Battle in 1925, captivating readers ever since.",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/9.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/9.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "304",
@@ -159,7 +159,7 @@ export default [
       "In the tranquil village of Kings Abbott, the death of millionaire Roger Ackroyd triggers a mystery following two previous murders. Narrator Dr. Sheppard, alerted by his sister to potential connections between the deaths involving poisoning and suicide, attracts the curiosity of retired detective Hercule Poirot on vacation. The Tunisian dagger, part of Ackroyd's collection, becomes crucial. Agatha Christie's narrative unveils a web of relationships and secrets, showcasing the author's genius in crafting engaging plots. Poirot unravels the mysteries and reveals the murderer in The Murder of Roger Ackroyd",
     shortDescription:
       "Threats, blackmail, vices, and a revealing letter challenge Poirot in an intriguing plot, marking his return to investigations.",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/15.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/10.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "296",
@@ -176,7 +176,7 @@ export default [
       "Hercule Poirot is entangled in mystery when a man invades his apartment under suspicious circumstances. Facing questions about the intruders identity, his state of shock, and the enigmatic meaning of a paper covered with the number four, Poirot is drawn into a dangerous web of intrigue. The renowned Belgian detective risks his life in the pursuit of truth about an international criminal organization. The engaging plot immerses Poirot in a dark world of conspiracies, challenging him to decipher secrets and confront dangers while unraveling the intertwined mysteries of this captivating narrative.",
     shortDescription:
       "Threats, blackmail, vices, and revelations challenge Poirot in an intriguing plot, marking his return to investigations.",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/11.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/11.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "208",
@@ -193,7 +193,7 @@ export default [
       "On the luxurious Blue Train to Nice, wealthy heiress Ruth Kettering is found dead, her rubies stolen. Husband Derek is the prime suspect, but Hercule Poirot has doubts. He meticulously reconstructs the crime and the journey, uncovering the killer on board. Exploring the intricate plot reveals hidden secrets and motivations among the passengers, challenging appearances. Agatha Christie masterfully weaves an engaging mystery in The Mystery of the Blue Train, keeping readers intrigued until the surprising revelation of the true culprit and their reasons.",
     shortDescription:
       "Perhaps you could love a thief, mademoiselle, but not a murderer!",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/12.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/12.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "272",
@@ -210,7 +210,7 @@ export default [
       "Constantly late for breakfast at Chimneys, Gerry Wade faces an innocent prank involving eight clocks. However, the joke takes a tragic turn, requiring young Bundle and her friends to unravel the mystery behind the unexpected event. The narrative explores the intriguing dynamics of characters and their reactions to the unforeseen, while the plot unfolds in pursuit of the truth. The author's skill in creating surprising situations and captivating characters makes the story an engaging read, blending suspense, humor, and investigation in a peculiar and fascinating context.",
     shortDescription:
       "Agatha Christie challenges detectives in The Seven Dials Mystery, where clocks reveal secrets, unleashing captivating suspense.",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/13.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/13.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "288",
@@ -244,7 +244,7 @@ export default [
       "In the serene village of St. Mary Mead, a brutal crime disrupts the community after fifteen years. Inspector Slack, the arrogant investigator, leads the case. The discreet Miss Marple, a gardening and bird enthusiast, reveals her true passion: studying human behavior. Intrigued by the mystery, this unlikely detective employs her unique insight to unravel secrets in the village's corners. Miss Marple emerges as a brilliant solver, unveiling Agatha Christie's intricate plot in a captivating narrative that enthralls even the most discerning readers.",
     shortDescription:
       "Hercule Poirot unravels a murder in the Vicarage, revealing secrets in Agatha Christie's intricate plot.",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/15.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/14.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "264",
@@ -261,7 +261,7 @@ export default [
       "In the intriguing atmosphere of a New Year's party, millionaire Mr. Satterthwaite is surprised by the mysterious arrival of Harley Quin. This enigmatic man, whose presence heralds either love or death, unpredictably alters the course of the night. Satterthwaite, a keen observer, perceives the duality of Quins influences. The peculiar character, appearing and disappearing swiftly, leaves a trail of uncertainty and suspense. Interactions between Satterthwaite and Quin reveal an intricate game of destinies, where mystery intertwines with the unexpected, marking the party with a touch of fate and intrigue.",
     shortDescription:
       "In intriguing tales, Agatha Christie's favorite detectives join forces, unraveling mysteries with observational skills and mystical instincts.",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/16.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/16.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "288",
@@ -278,7 +278,7 @@ export default [
       "In Sittaford, a pre-storm seance unveils murder, transforming diversion into a dark puzzle. Christie weaves suspense in an atmospheric, remote locale, with an impending storm and a supernatural twist. The interactions, isolated setting, and paranormal element create an intriguing mystery, leaving readers eager to uncover the hidden truth amid the looming storm's suspenseful backdrop. As the plot twists and turns, Agatha Christie masterfully builds anticipation, skillfully engaging readers with the enigmatic circumstances. The suspenseful narrative in this remote setting captivates, showcasing Christie's ability to craft a compelling mystery filled with twists and turns.",
     shortDescription:
       "Agatha Christie astounds with an impossible crime: prank or supernatural warning? Her ingenious novel explores mystery and unexpected surprises.",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/17.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/17.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "218",
@@ -295,7 +295,7 @@ export default [
       "On the sunny Cornwall coast, Poirot plans retirement but uncovers mystery with threatened heiress Miss Buckley. Apparent accidents conceal a sinister truth. Excited, Poirot postpones retirement, diving into another challenging case testing his deductive skills. The plot blends the promise of rest with an unexpected twist, showcasing the unparalleled talent of the Queen of Crime in crafting engaging and unpredictable stories. With Captain Hastings by his side, Poirot faces a challenge that unveils the truth behind seemingly casual events, leading to a surprising and satisfying conclusion.",
     shortDescription:
       "Published in 1932, featuring Belgian detective Hercule Poirot and his friend, Arthur Hastings.",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/18.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/18.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "224",
@@ -312,7 +312,7 @@ export default [
       "In the captivating Tuesday Night Club, Agatha Christie presents her second collection of short stories. Published by HarperCollins, the group of friends, led by Miss Marple, gathers weekly to unravel intriguing mysteries. Each of the thirteen stories tackles an unsolved enigma, from a thief repeating the same crime to cryptic messages of poisoning. With her keen insight, Miss Marple guides the meticulous analysis of these challenging cases. The book immerses readers in a suspenseful plot, featuring charismatic characters and challenges that test their amateur detective skills.",
     shortDescription:
       "In The Thirteen Problems, Agatha Christie's ingenuity in engaging suspense stories surprises, captivating the reader.",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/19.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/19.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "256",
@@ -329,7 +329,7 @@ export default [
       "In Miss Jane Marple, actress Jane Wilkinson asks Poirot to rid her of her husband. When Lord Edgware is murdered, Jane becomes the prime suspect. With a solid alibi, Poirot faces the challenge of uncovering the true killer among the twelve dinner guests. The plot involves intrigues and twists, requiring Poirot to use all his cunning to prevent further victims. The intricate storyline takes readers on a suspenseful journey, questioning alibis and revealing hidden motives. Miss Jane Marple highlights Agatha Christie's brilliance in crafting complex mysteries and intriguing characters.",
     shortDescription:
       "Released in 1933, Thirteen at Dinner stands out with a challenging case for Poirot, revealing Agatha Christie's ingenious mystery",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/20.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/20.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "240",
@@ -346,7 +346,7 @@ export default [
       "After a disastrous golf shot, Bobby Jones finds a dying man near the cliff. The intriguing last words, 'Why didn't they ask Evans?', trigger a quest for the truth. Bobby and his friend Frankie dive into a mystery involving false identities, murders, and buried secrets. The search for Evans unveils a complex web of deceit, connecting unexpected dots and revealing shocking truths. With skill, Agatha Christie weaves a captivating plot, keeping readers eager to unravel the meaning of the enigmatic last words and unearth the secrets in the deadly game of intrigue.",
     shortDescription:
       "Bobby and Frankie investigate Evans and the mysterious demand, uncovering real dangers despite not being professional detectives.",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/21.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/21.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "256",
@@ -363,7 +363,7 @@ export default [
       "In Parker Pyne's intriguing plot, an advertisement offers consultations for happiness. After decades in bureaucracy, the detective redirects his skills not to solve crimes but to unravel human desires. Traveling incognito, he not only transforms lives, saving marriages and adding adventure but also encounters thieves and uncovers murders. Far from unsolvable puzzles, Pyne explores the depths of the human heart, providing thrilling twists and surprising revelations. A unique approach to suspense, where the real mystery lies in the complexities of feelings and the personal transformations triggered by the insightful Parker Pyne.",
     shortDescription:
       "Published in 1934, a collection of fourteen heartwarming stories featuring Parker Pyne, hailed by The Observer for its emotional insight.",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/22.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/22.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "272",
@@ -380,7 +380,7 @@ export default [
       "The story follows a destitute bourgeois family leasing a splendid house at an unexpectedly low cost. The dwelling, owned by the elusive Lord Listerdale, has been vacant for years without explanation for his disappearance. The young tenant speculates that the affordable rent might be linked to a potential murder, suspecting Lord Listerdale's hidden body within the premises. As the plot thickens, intriguing coincidences emerge, guiding the young tenant through a captivating journey to unravel the mystery behind Lord Listerdale's vanishing act. The storyline unveils unexpected twists, maintaining suspense until the revelation of the hidden truth.",
     shortDescription:
       "Everyone shares one thing: Agatha Christie's skillful hand.",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/23.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/23.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "320",
@@ -397,7 +397,7 @@ export default [
       "In a crooked mansion on the outskirts of London, millionaire Aristide Leonides is poisoned. The house filled with relatives becomes a field of suspicions, especially against the young widow. The police investigate, but discord arises. Sophia, the deceased's granddaughter, joins her boyfriend to unravel the mystery of her grandfather's death, diving into a plot full of family intrigues and tension. The intricate plot of 'Crooked House' challenges the characters to confront secrets and revelations, creating a captivating thriller that keeps the reader eager for answers until the surprising conclusion.",
     shortDescription:
       "In a crooked mansion, millionaire Aristide Leonides is poisoned, sparking suspicions among relatives. Sophia, his granddaughter, investigates family intrigues.",
-    imageUrl: "https://github.com/TaGoulart/SAP012-dataverse/main/src/images/books/24.jpg",
+    imageUrl: "https://raw.githubusercontent.com/TaGoulart/SAP012-dataverse/main/src/images/books/24.jpg",
     facts: {
       authorBook: "Agatha Christie",
       pagesBook: "240",
