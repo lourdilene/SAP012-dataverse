fb8028d9e1820723285c3d66b1a600996972b864
Lourdilene Souza <lourdilene.souza@gmail.com>
2024-01-16 12:22:42 -0300

    acrescimo de microdados

--- src/view.js
+++ src/view.js
@@ -5,23 +5,48 @@ export const renderItems = (data) => {
     const liElement = document.createElement("li");
     liElement.classList.add("books");
 
-    liElement.setAttribute("itemscope", "");
-    liElement.setAttribute("itemtype", "http://schema.org/Book");
+    const dlElement = document.createElement("dl");
 
-    const titleElement = document.createElement("h2");
-    titleElement.textContent = book.name;
-    titleElement.setAttribute("itemprop", "name");
-    liElement.appendChild(titleElement);
+    dlElement.setAttribute("itemscope", "");
+    dlElement.setAttribute("itemtype", "http://schema.org/Book");
 
-    const imgElement = document.createElement("img");
-    imgElement.src = book.imageUrl;
-    imgElement.setAttribute("itemprop", "image");
-    liElement.appendChild(imgElement);
+    const dtElement = document.createElement("dt");
+    dtElement.textContent = book.name;
 
-    const descriptionElement = document.createElement("p");
-    descriptionElement.textContent = book.shortDescription;
-    descriptionElement.setAttribute("itemprop", "description");
-    liElement.appendChild(descriptionElement);
+    const ddElement = document.createElement("dd");
+    ddElement.setAttribute("itemprop", "name");
+    ddElement.textContent = book.name;
+
+    const dtSElement = document.createElement("dt");
+    dtSElement.textContent = book.shortDescription;
+
+    const ddSElement = document.createElement("dd");
+    ddSElement.setAttribute("itemprop", "name");
+    ddSElement.textContent = book.shortDescription;
+
+    dlElement.appendChild(dtElement, ddElement);
+
+    liElement.appendChild(dlElement);
+
+    dlElement.appendChild(dtSElement, ddSElement);
+
+    // liElement.setAttribute("itemscope", "");
+    // liElement.setAttribute("itemtype", "http://schema.org/Book");
+
+    // const titleElement = document.createElement("h2");
+    // titleElement.textContent = book.name;
+    // titleElement.setAttribute("itemprop", "name");
+    // liElement.appendChild(titleElement);
+
+    // const imgElement = document.createElement("img");
+    // imgElement.src = book.imageUrl;
+    // imgElement.setAttribute("itemprop", "image");
+    // liElement.appendChild(imgElement);
+
+    // const descriptionElement = document.createElement("p");
+    // descriptionElement.textContent = book.shortDescription;
+    // descriptionElement.setAttribute("itemprop", "description");
+    // liElement.appendChild(descriptionElement);
     ulElement.appendChild(liElement);
   });
 
