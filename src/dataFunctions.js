17f642be16005f0ed8f0ea09d599c9fae0b02859
Lourdilene Souza <lourdilene.souza@gmail.com>
2024-01-17 19:34:14 -0300

    finish sort

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,7 +1,20 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
+export const sortByPagesAscending = (data) => {
+  const pagesAscending = (a, b) => {
+    return parseInt(a.facts.pagesBook) - parseInt(b.facts.pagesBook);
+  };
 
-export const example = () => {
-  return 'example';
+  const booksAscending = data.slice().sort(pagesAscending);
+  return booksAscending;
+};
+
+export const sortByPagesDescending = (data) => {
+  const pagesDescending = (a, b) => {
+    return parseInt(b.facts.pagesBook) - parseInt(a.facts.pagesBook);
+  };
+
+  const booksDescending = data.slice().sort(pagesDescending);
+  return booksDescending;
 };
 
 export const anotherExample = () => {
--- src/main.js
+++ src/main.js
@@ -1,7 +1,33 @@
-// import { example } from "./dataFunctions.js";
+import {
+  sortByPagesAscending,
+  sortByPagesDescending,
+} from "./dataFunctions.js";
 import { renderItems } from "./view.js";
 
 import data from "./data/dataset.js";
 
+const sortSelectElement = document.getElementById("sorts");
 const rootElement = document.getElementById("root");
-rootElement.appendChild(renderItems(data));
+let processedData = [];
+
+sortSelectElement.addEventListener("change", function () {
+  const selectedValue = sortSelectElement.value;
+
+  if (selectedValue === "number-of-pages-ascending") {
+    processedData = sortByPagesAscending(data);
+  }
+  if (selectedValue === "number-of-pages-descending") {
+    processedData = sortByPagesDescending(data);
+  }
+  if (selectedValue === "featured") {
+    processedData = data;
+  }
+
+  rootElement.innerHTML = "";
+
+  rootElement.appendChild(renderItems(processedData));
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  rootElement.appendChild(renderItems(data));
+});
